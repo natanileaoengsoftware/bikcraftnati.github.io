b181487f0d893f17da1c13bdffc20c069f76fdf1
Natani Leão <natanigselau@gmail.com>
2023-07-21 21:45:25 -0300

    otimizacoes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const links = document.querySelectorAll('.header-menu a');
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
 
   if (url.includes(href)) {
-   link.classlist.add('ativo');
-  }   
+    link.classList.add("ativo");
+  }
 }
 links.forEach(ativarLink);
 
@@ -20,7 +20,7 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 const perguntas = document.querySelectorAll(".perguntas button");
-function ativarPergunta(event){
+function ativarPergunta(event) {
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
@@ -31,6 +31,26 @@ function ativarPergunta(event){
 }
 
 function eventosPerguntas(pergunta) {
-pergunta.addEventListener('click', ativarPergunta);
+  pergunta.addEventListener("click", ativarPergunta);
 }
 perguntas.forEach(eventosPerguntas);
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+const img = event.currentTarget;
+ const media = matchMedia("(min-width:1000px)").matches;
+ if(media){
+galeriaContainer.prepend(img);
+ }
+
+}
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+galeria.forEach(eventosGaleria);
+
+if(window.SimpleAnime){
+new SimpleAnime()
+}
